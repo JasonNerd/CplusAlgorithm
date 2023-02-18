7fd37206b18900748d5996000f442c1dd7303250
JasonNerd <duan912rainbow@qq.com>
2023-02-18 22:36:24 +0800

    更新了两个题目的描述(未实现)

--- sunnyWhy/graphAlg/GA02OptimPath/GA02Pemergency.cpp
+++ sunnyWhy/graphAlg/GA02OptimPath/GA02Pemergency.cpp
@@ -1,18 +1,42 @@
 /*
-5602
-12153
-011
-022
-378
-第10章提高篇(4)-图算法专题
-031
-121
-241
-341
-输出样例
-24
-题意
-给出N个城市，M条无向边。每个城市中都有一定数目的救援小组，所有边的边权已知。
-现在给出起点和终点，求从起点到终点的最短路径条数及最短路径上的救援小组数目之和。
-如果有多条最短路径，则输出数目之和最大的。
+As an emergency rescue team leader of a city, you are given a special map of your country.
+The map shows several scatered cities connected by some roads. 
+Amount of rescue teams in each city and the length of each road between any pair of cities are marked on the map.
+When there is anemergency call to you from some other city, your job is to lead your men to the place as quickly aspossible,
+and at the mean time, call up as many hands on the way as possible.
+输入格式
+Each input file contains one test case. For each test case, the first line contains 4 positiveintegers: 
+N (<500)-the number of cities (and the cities are numbered from 0 to N-1), 
+M--the number of roads, C1 and C2--the cities that you are currently in and that you must save,respectively. 
+The next line contains N integers, where the i-th integer is the number of rescue teams in the i-th city
+Then M lines follow, each describes a road with three integers cl, c2 and L, 
+which are the pair of cities connected by a road and the length of that road, respectively. 
+It is guaranteed that there exists at least one path from C1 to C2.
+输出格式
+For each test case, print in one line two numbers: 
+the number of different shortest pathsbetween C1 and C2, and the maximum amount of rescue teams you can possibly gather.
+All the numbers in a line must be separated by exactly one space, and there is no extra space allowed at the end of a line.
+*/
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
+
+/*
+input example:
+5 6 0 2
+1 2 1 5 3
+0 1 1
+0 2 2
+0 3 1
+1 2 1
+2 4 1
+3 4 1
+output example:
+2 4
+
 */
--- sunnyWhy/graphAlg/GA02OptimPath/GA02Ptravelplan.cpp
+++ sunnyWhy/graphAlg/GA02OptimPath/GA02Ptravelplan.cpp
@@ -1,13 +1,42 @@
 /*
-输入样例
-4503
-01120
+题目描述
+A traveler's map gives the distances between cities along the highways, together with the cost of each highway.
+Now you are supposed to write a program to help a traveler to decide the shortest path between his/her /
+starting city and the destination. If such a shortest path is not unique, you aresupposed to output the one /
+with the minimum cost, which is guaranteed to be unique.
+输入格式
+Each input file contains one test case. Each case starts with a line containing 4 positive integers N,M, S, and D, 
+where N (<500) is the number of cities (and hence the cities are numbered from 0to N-1); 
+M is the number of highways; S and D are the starting and the destination cities.respectively. 
+Then M lines follow, each provides the information of a highway, in the format:
+City1 City2 Distance Cost
+where the numbers are all integers no more than 500, and are separated by a space.
+输出格式
+For each test case, print in one line the cities along the shortest path from the starting point tothe destination,
+followed by the total distance and the total cost of the path. T
+he numbers must be separated by a space and there must be no extra space at the end of output.
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
+
+/*
+input
+4 5 0 3
+0 1 1 20
 1 3 2 30
-03410
-02220
-23 120
-输出样例
-023340
+0 3 4 10
+0 2 2 20
+2 3 1 20
+output
+0 2 3 3 40
 题意
-有N个城市(编号为0~N-1)、M 条道路(无向边)，并给出 M条道路的距离属性与花费属性。现在给定起点 S 与终点 D，求从起点到终点的最短路径、最短距离及花费。注意:如果有多条最短路径，则选择花费最小的那条。
+有N个城市(编号为0~N-1)、M 条道路(无向边)，并给出 M条道路的距离属性与花费属性。
+现在给定起点 S 与终点 D，求从起点到终点的最短路径、最短距离及花费。
+注意:如果有多条最短路径，则选择花费最小的那条。
 */
